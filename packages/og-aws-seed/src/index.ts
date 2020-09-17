862b609d3eba7e5991db188c7e5b2b9f8e7663ea
kevinslin <kevin@thence.io>
2020-09-17 09:06:51 -0700

    chore: update og-aws names

--- packages/og-aws-seed/src/index.ts
+++ packages/og-aws-seed/src/index.ts
@@ -1,14 +1,19 @@
+import { normalizeServiceName } from "@dendronhq/aws-constants/lib/src";
 import { Note } from "@dendronhq/common-all";
+import { cleanFileName } from "@dendronhq/common-server";
 import { DendronSeed, PrepareOpts } from "@dendronhq/seeds-core";
 import fs from "fs-extra";
 import path from "path";
+import remark from "remark";
+import markdownParse from "remark-parse";
+import { getOutput, ogAWSPlugin } from "./parseOgAWS";
 
 export default class OgAWSSeed extends DendronSeed {
   config() {
     return {
       src: {
         type: "git" as const,
-        url: "https://github.com/AwsGeek/awsgeek.github.io",
+        url: "https://github.com/open-guides/og-aws.git",
       },
     };
   }
@@ -17,38 +22,39 @@ export default class OgAWSSeed extends DendronSeed {
     const ctx = "prepare";
     this.L.info({ctx, opts});
     const { root } = opts;
-    const fpath = path.join(root, "images");
-    const dataPath = fs.readdirSync(fpath);
-    dataPath.map(ent => {
-      console.log(ent);
+    const fpath = path.join(root, "README.md");
+    const dataPath = fs.readFileSync(fpath, { encoding: "utf8" });
+    remark()
+      .use(markdownParse, { gfm: true })
+      .use(ogAWSPlugin)
+      .processSync(dataPath);
+    const output = getOutput();
+    const dataSplit = dataPath.split("\n");
+    // skip last one, high availability
+    const notes = output.slice(0, -1).map((ent, index) => {
+      const { name, payload } = ent;
+      const nname = normalizeServiceName(name);
+      let start = payload.position?.start.line as number;
+      start += 1;
+      let end = output[index + 1]?.payload.position?.start.line || -1;
+      end -= 1;
+      const content = [`# ${nname}`].concat(dataSplit.slice(start, end));
+      const cleanName = cleanFileName(nname.toLowerCase()).replace(/,/g, "");
+      const fname = `s.${cleanName}`;
+      return new Note({
+        id: cleanName,
+        created: "0",
+        updated: "0",
+        fname,
+        body: content.join("\n"),
+        custom: {
+          source: {
+            name: "og-aws",
+            url: "https://github.com/open-guides/og-aws"
+          }
+        }
+      });
     });
-    let notes: Note[] = [];
-
-    // const notes = output.slice(0, -1).map((ent, index) => {
-    //   const { name, payload } = ent;
-    //   let start = payload.position?.start.line as number;
-    //   start += 1;
-    //   let end = output[index + 1]?.payload.position?.start.line || -1;
-    //   end -= 1;
-    //   const nname = normalizeServiceName(name);
-    //   const content = [`# ${nname}`].concat(dataSplit.slice(start, end));
-    //   const cleanName = cleanFileName(nname.toLowerCase()).replace(/,/g, "");
-    //   const fname = `s.${cleanName}`;
-    //   return new Note({
-    //     id: cleanName,
-    //     title: nname,
-    //     created: "0",
-    //     updated: "0",
-    //     fname,
-    //     body: content.join("\n"),
-    //     custom: {
-    //       source: {
-    //         name: "og-aws",
-    //         url: "https://github.com/open-guides/og-aws"
-    //       }
-    //     }
-    //   });
-    // });
     return notes;
   }
 }
